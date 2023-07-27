c290358595e070a7b4e6adcd3b6280f7a87022b2
Tiago Leite <contato@tiagocreator.com>
2023-07-27 20:49:18 -0300

    Add contact page scripts

--- src/script.js
+++ src/script.js
@@ -22,9 +22,32 @@ navbarLinks.forEach((link) => link.addEventListener('click', closeNavbar));
 window.addEventListener('scroll', () => {
   if (window.scrollY > 100) {
     headerBottom.classList.add('active');
-    backToTopBtn.classList.add('active');
+    if (backToTopBtn) {
+      backToTopBtn.classList.add('active');
+    }
   } else {
     headerBottom.classList.remove('active');
-    backToTopBtn.classList.remove('active');
+    if (backToTopBtn) {
+      backToTopBtn.classList.remove('active');
+    }
   }
 });
+
+//Contact form 'fake sent' confirmation script
+const form = document.querySelector('.contact-form');
+const formText = document.querySelector('.form-text');
+const formContent = document.querySelector('.form-content');
+const sendFormBtn = document.querySelector('.send-form');
+const sentIcon = '../src/assets/img/sent.png';
+const submitForm = (e) => {
+  e.preventDefault();
+  formText.style.display = 'none';
+  formContent.innerHTML = `
+    <div>
+      <p>Obrigado por preferir nossos serviços. Um atendente irá entrar em contato com você em breve pelo número de telefone. O horário de atendimento é de segunda a sexta das 08:00 as 18:00.</p>
+      <img class='sent-icon' src='${sentIcon}' alt='' />
+    </div>
+  `;
+  sendFormBtn.style.display = 'none';
+};
+form.addEventListener('submit', submitForm);
